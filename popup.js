533a9ff4b5bdb664501cc9b4d30d0bb96f0e9982
Aribk7 <aribkhan2004@gmail.com>
2023-03-13 08:45:16 -0700

    co,mmit

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // replace dom with buildspace.so
+  
+});
+
+
+// chrome.tabs.onCreated.addListener(function (tab) {
+//     chrome.tabs.update(tab.id, {
+//         url: "https://tooltips.ai"
+//     });
